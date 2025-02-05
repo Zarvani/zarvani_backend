409200927926508dc9ed6a516d7c0de890d68815
Shuru63 <shubhamkuargarg63@gmail.com>
2025-02-06 02:54:20 +0530

    update some Schema

--- ZarvaniApp-be/Controller/Usercontroller.js
+++ ZarvaniApp-be/Controller/Usercontroller.js
@@ -6,7 +6,6 @@ const { OAuth2Client } = require('google-auth-library');
 const jwt = require("jsonwebtoken");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { use } = require("../Route/UserRouter");
-const SelectedDoner = require("../Model/selectedDoner");
 const { applyAdditionalFilters } = require('../Utills/filterControl'); 
 const { filterWorkerByLocation } = require('../Utills/filterControl'); 
 
@@ -60,7 +59,7 @@ const createUser = async (req, res) => {
 };
 const Loginuser = async (req, res) => {
     let { email, password } = req.body;
-
+     console.log( email, password)
     if (!email || !password) {
         return res.status(500).json({
             success: false,
