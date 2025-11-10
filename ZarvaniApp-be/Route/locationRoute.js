7c977a4df712318d3a39c61c98d490c410cb9c54
Shubham kumar garg <121496598+Shuru63@users.noreply.github.com>
2025-11-11 01:20:45 +0530

    Delete ZarvaniApp-be directory

--- ZarvaniApp-be/Route/locationRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { updateLocationIfChanged} = require('../Controller/LocationController');
-const { Authentication, AuthorizeRole } = require("../Middleware/Authentication")
-const router = express.Router();
-
-
-router.route('/update-location').post(Authentication,updateLocationIfChanged);
-
-module.exports = router;
